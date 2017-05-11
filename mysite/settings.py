9f8cdf8a44b235bc192f6ad608b1f2d78f028a46
khanhhoa <zero.hoa.52@gmail.com>
2017-05-11 22:39:50 +0700

    change to using pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#0-b0q+=h#1p6+xj2z722ga2qhqx_iz^aws#6bux9s@2-cf4-!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
