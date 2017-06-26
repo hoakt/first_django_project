cabf8a6c74310cdc02e8b29e0434a9448e20ec4c
khanhhoa <zero.hoa.52@gmail.com>
2017-06-26 23:50:57 +0700

    Homework: Adding security to your website

--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post # import our Post to views
 from .forms import PostForm # import our form to views
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # 'request' is everything we receive from user via internet
@@ -15,19 +16,21 @@ def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'myapp/post_detail.html', {'post': post})
 	
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST) # new fields' data from new post are now in request.POST
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+#			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
 	else:
 		form = PostForm()
 	return render(request, 'myapp/post_edit.html', {'form': form})
-	
+
+@login_required	
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method == "POST":
@@ -35,9 +38,26 @@ def post_edit(request, pk):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
-			post.published_date = timezone.now()
+#			post.published_date = timezone.now()
 			post.save()
-			return redirect('post_detail', pk=post.pk)
+			return redirect('post_detail', pk=pk)
 	else:
 		form = PostForm(instance=post)
-	return render(request, 'myapp/post_edit.html', {'form': form})	
+	return render(request, 'myapp/post_edit.html', {'form': form})
+
+@login_required	
+def draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'myapp/draft_list.html', {'posts': posts})
+
+@login_required	
+def post_publish(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	post.publish()
+	return redirect('post_detail', pk=pk)
+
+@login_required	
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+#login page redirection
+#when the login page is accessed directly, it will redirect a successful login to the top-level index
+LOGIN_REDIRECT_URL = '/'
