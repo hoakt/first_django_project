96dbf4de6e7d1f961265516b2ea425b09794ef1f
khanhhoa <zero.hoa.52@gmail.com>
2017-07-08 00:19:52 +0700

    Homework: create comment model and authorize for approve/remove comment.

--- myapp/urls.py
+++ myapp/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
 	url(r'^drafts/$', views.draft_list, name='draft_list'),
 	url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
 	url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment, name='add_comment'),
+	url(r'^comment/(?P<pk>\d+)/approve/$', views.comment_approve, name='comment_approve'),
+	url(r'^comment/(?P<pk>\d+)/remove/$', views.comment_remove, name='comment_remove'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Post # import our Post to views
-from .forms import PostForm # import our form to views
+from .models import Post, Comment # import our Post, Comment model to views
+from .forms import PostForm, CommentForm # import our form to views
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 # 'myapp/post_list.html' is giving template file; {} is a place in which we can add some things for the template to use
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'myapp/post_list.html', {'posts': posts})
 	
 def post_detail(request, pk):
@@ -61,3 +61,28 @@ def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.delete()
     return redirect('post_list')
+	
+def add_comment(request,pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = CommentForm(request.POST) # use CommentForm to create a form
+		if form.is_valid():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = CommentForm()
+	return render(request, 'myapp/add_comment_form.html', {'form': form})
+	
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
